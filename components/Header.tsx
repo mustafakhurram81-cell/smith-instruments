b603d398c6cce11564891bf892a83dc48557d6ef
mustafakhurram81-cell <mustafakhurram81@gmail.com>
2026-02-05 17:40:32 +0500

    Reduce header logo size

--- components/Header.tsx
+++ components/Header.tsx
@@ -46,7 +46,7 @@ export const Header: React.FC = () => {
                     {/* Left: Logo */}
                     <div className="flex-1 flex items-center">
                         <div className="flex items-center gap-3 cursor-pointer group" onClick={() => navigate('/')}>
-                            <div className="h-16 w-64 relative overflow-hidden flex items-center">
+                            <div className="h-12 w-48 relative overflow-hidden flex items-center">
                                 <img
                                     src={logoTransparent}
                                     alt="Smith Instruments"
